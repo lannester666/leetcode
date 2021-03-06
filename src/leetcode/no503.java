1b505c1155519f26e382d6700ca88f74874259db
T20210287 <806702572@qq.com>
2021-03-06 11:27:13 +0800

    3.6

--- src/leetcode/no503.java
+++ src/leetcode/no503.java
@@ -1,5 +1,53 @@
 package leetcode;
 
-public class no503 {
+import java.util.Stack;
 
+public class no503 {
+    public int[] nextGreaterElements(int[] nums) {
+        int n=nums.length;
+        int[] res=new int[n];
+        Stack<Integer> stack=new Stack<>();
+        int max=0,index=0;
+        for(int i=0;i<n;i++)
+            if(nums[i]>max)
+            {
+                max=nums[i];
+                index=i;
+            }
+        res[index]=-1;
+        stack.push(res[index]);
+        index--;
+        int count=1;
+        while(count<n)
+        {
+            boolean flag=false;
+            if(index<0)
+                index+=n;
+            while(!stack.empty())
+            {
+                int tp=stack.peek();
+                if(tp>nums[index])
+                {
+                    res[index]=tp;
+                    stack.push(nums[index]);
+                    flag=true;
+                    break;
+                }
+                else
+                    stack.pop();
+            }
+            if(!flag)
+            {
+                stack.push(nums[index]);
+                res[index]=-1;
+            }
+            count++;index--;
+        }
+        return res;
+    }
+    public static void main(String[] args)
+    {
+        no503 t=new no503();
+        t.nextGreaterElements(new int[]{1,2,3,4,5});
+    }
 }
